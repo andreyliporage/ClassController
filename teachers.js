f2b0f658bc9552c19077f3091d94847518c1f3ef
andreyliporage <andreyliporage@gmail.com>
2020-03-31 18:32:50 -0300

    update

--- teachers.js
+++ teachers.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require("fs")
+const data = require("./data.json")
 
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -8,9 +9,13 @@ exports.post = function(req, res) {
             return res.send("Preencha todos os campos.")
         }
     }
-    
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
-        if (err) return res.send("Write File error!")
+
+    data.teachers.push(req.body)
+
+    // Lembrar que: desse jeito apagará tudo no arquivo. Ficará sempre só 1 dado
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+
+        if(err) return res.send("Write File error!")
 
         return res.redirect("/teachers")
     })
